dfb8666d09e93da0b93eb9ca68120d9ee56f5ab1
Sajjad <sdyussuf@gmail.com>
2024-12-05 10:46:57 +0330

    Initial commit

---
+++ src/main/java/com/sajjady/mynetwork/MyNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.sajjady.mynetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyNetworkApplication.class, args);
+    }
+
+}
